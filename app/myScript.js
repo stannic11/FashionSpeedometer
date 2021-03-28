7d9840f398799f9ca5aaba91e28f43b4edd5540c
stannic11 <78308786+stannic11@users.noreply.github.com>
2021-03-27 20:28:27 -0400

    Delete app directory

--- app/myScript.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    document.getElementById("rateA").innerHTML = "3 / 5";
-    document.getElementById("rateB").innerHTML = "4 / 5";
-    document.getElementById("rateC").innerHTML = "3 / 5";
-    document.getElementById("rateD").innerHTML = "4 / 5";
-}
